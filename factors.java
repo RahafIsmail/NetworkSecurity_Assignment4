be2e0d75c73b7615d9ed2ef48074a5c1ce86ffe7
RahafIsmail <95977849+RahafIsmail@users.noreply.github.com>
2023-05-15 13:01:22 +0300

    Create factors.java

---
+++ factors.java
@@ -0,0 +1,73 @@
+//I wrote a function to display all the factors if the number is not prime
+import java.util.Scanner;
+public class try_ass4 {
+    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         while (true) {
+             
+             System.out.println("***************************");
+             System.out.println("Prime Number Checker");
+             System.out.println("***************************");
+             System.out.println("0- EXIT!");
+             System.out.println("1- CHECK!");
+             System.out.print("ENTER YOUR CHOICE:");
+             int choice = scanner.nextInt();
+             int number = 0;
+             
+             switch (choice) {
+             case 0:
+              System.out.println("EXIT THE PROGRAM!!!");
+              scanner.close();
+               return;
+               
+              case 1:
+              System.out.print("Please Enter a Number: ");
+               number = scanner.nextInt();
+               if (number == 0) {
+               break;
+                }
+               else{
+               //Here I will call my functions in order to check the number and return the number of iterations 
+              if(PrimeNum(number)==1){
+              System.out.println(number + " is prime number" );
+              }
+              else{
+              System.out.println(number + "  is composite number and factors are -> " +factor(number));
+             // System.out.println("factors are: " + factor(number));
+                }
+               }
+             }
+         }
+     }
+    public static int PrimeNum(int num) {
+        if (num <= 1) {
+            return 0;
+        }
+        else{
+        for (int i = 2; i < num; i++) {
+         if (num % i == 0) {
+          return 0;   
+            }
+        }
+        
+     }
+         return 1;
+    }
+    
+       public static String factor(int number) {
+    String f = "" ;
+
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            f=f+ i + ",";
+        }
+    }
+    if(!f .isEmpty()){ // Remove the , if there are numbers
+        f  = f .substring(0, f .length() - 1);
+    }
+
+    return f ;
+}
+     }
+
